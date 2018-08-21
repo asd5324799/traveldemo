09d8993fced6bdf4575a207b58bf293aa15c1aca
yuanshuai <13606068001@163.com>
2018-08-21 17:54:22 +0800

    icons

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,11 +2,13 @@
   <div>
     <home-header></home-header>
     <home-swiper :list="list"></home-swiper>
+    <home-icons :list="list"></home-icons>
   </div>
 </template>
 <script>
   import HomeHeader from '../../components/header/header.vue'
   import HomeSwiper from '../../components/swiper/swiper.vue'
+  import HomeIcons from '../../components/icons/icons.vue'
 
   export default {
     data() {
@@ -16,7 +18,8 @@
     },
     components: {
       HomeHeader,
-      HomeSwiper
+      HomeSwiper,
+      HomeIcons
     }
   }
 </script>
