b8dcdf4d0ebec6761856367aefd8a60e8ed99881
yuanshuai <13606068001@163.com>
2018-08-21 14:51:04 +0800

    beta 0.1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+// 移动端300ms延迟解决方案
+fastClick.attach(document.body)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
